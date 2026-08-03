6f7140ef61105aaab8f9509494d0d21569641150
hharsha166-dev <hharsha166@gmail.com>
2026-08-03 10:26:32 +0530

    Update NewPanApplication.jsx

--- pages/NewPanApplication.jsx
+++ pages/NewPanApplication.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import api from '../api';
 
 const GENDER_OPTIONS = ['Male', 'Female', 'Transgender'];
